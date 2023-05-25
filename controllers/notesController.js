e9d0ce9099b64817ef573e51d95fe49da95d69a0
Frank-Colorado <chawnkiee@gmail.com>
2023-05-25 19:43:10 -0400

    added code to notesController.js file

---
+++ controllers/notesController.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const path = require("path");
+
+const getNotes = (req, res) => {};
+
+const createNote = (req, res) => {};
+
+const deleteNote = (req, res) => {};
+
+module.exports = {
+  getNotes,
+  createNote,
+  deleteNote,
+};
