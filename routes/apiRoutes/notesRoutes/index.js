d89064a8f5168626f85bd40f7965c09eb991fa27
Frank-Colorado <chawnkiee@gmail.com>
2023-05-25 19:46:49 -0400

    added comments to notesRoutes/index.js

--- routes/apiRoutes/notesRoutes/index.js
+++ routes/apiRoutes/notesRoutes/index.js
@@ -5,6 +5,15 @@ const {
   deleteNote,
 } = require("../../../controllers/notesController");
 
-router.route("/").get(getNotes).post(createNote).delete(deleteNote);
+// these routes will be called with /api/notes
+router
+  .route("/")
+  // GET request that returns all notes from the database
+  .get(getNotes)
+  // POST request that adds a new note to the database
+  .post(createNote);
+
+// DELETE request that deletes a note with a specific id
+router.route("/:id").delete(deleteNote);
 
 module.exports = router;
