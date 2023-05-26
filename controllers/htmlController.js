c344a8991ddb7687ff632e7ed1f64076a214931f
Frank-Colorado <chawnkiee@gmail.com>
2023-05-25 20:38:18 -0400

    created displayNotes function in htmlController

---
+++ controllers/htmlController.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const path = require("path");
+
+const displayNotes = (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "public", "notes.html"));
+};
+
+module.exports = { displayNotes };
