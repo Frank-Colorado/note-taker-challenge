6070eda958ba75707da4ea5703ea9b6993b0ea74
Frank-Colorado <chawnkiee@gmail.com>
2023-05-25 20:05:22 -0400

    added code for createNote function

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -17,7 +17,40 @@ const getNotes = (req, res) => {
   );
 };
 
-const createNote = (req, res) => {};
+const createNote = (req, res) => {
+  const { note } = req.body;
+  // fs.readFile() method reads the notes from db.json file
+  fs.readFile(
+    // path.join() method joins the path of the current file (notesController.js) with the path to the db.json file
+    path.join(__dirname, "..", "db", "db.json"),
+    "utf8",
+    (err, notes) => {
+      // check if there is an error
+      if (err) {
+        // if there is an error, the response status is set to 500 and the error is returned as json
+        res.status(500).json({ err });
+      }
+      // parse the notes from the db.json file
+      const parsedNotes = JSON.parse(notes);
+      // add the new note the user sent to the parsedNotes array
+      parsedNotes.push({ note });
+
+      // fs.writeFile() method writes the notes to db.json file
+      fs.writeFile(
+        // path.join() method joins the path of the current file (notesController.js) with the path to the db.json file
+        path.join(__dirname, "..", "db", "db.json"),
+        // JSON.stringify() method converts the parsedNotes array to JSON
+        JSON.stringify(parsedNotes, null, 2),
+        (err) => {
+          if (err) {
+            return res.status(500).json({ err });
+          }
+        }
+      );
+    }
+  );
+  res.json(note);
+};
 
 const deleteNote = (req, res) => {};
 
