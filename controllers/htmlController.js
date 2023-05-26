e110a6050a59c180a2e78e8f7916d85b338b902d
Frank-Colorado <chawnkiee@gmail.com>
2023-05-25 20:40:39 -0400

    fixed bug

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const displayNotes = (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "..", "public", "notes.html"));
+  res.sendFile(path.join(__dirname, "..", "public", "notes.html"));
 };
 
 module.exports = { displayNotes };
