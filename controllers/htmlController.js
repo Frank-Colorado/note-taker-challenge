bd3c698d9bc3fe47e73dc488925c68bd3f1a36f1
Frank-Colorado <chawnkiee@gmail.com>
2023-05-25 21:19:53 -0400

    commented htmlController.js

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 
+// This is a function called display notes that will display the notes.html file
 const displayNotes = (req, res) => {
+  // sendFile is a method that will send the file at the given path
   res.sendFile(path.join(__dirname, "..", "public", "notes.html"));
 };
 
