5c736152d16c6cc4756ed14fc60f045a6dd3231c
Frank-Colorado <chawnkiee@gmail.com>
2023-05-25 20:31:45 -0400

    added code to deleteNote function

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -56,8 +56,42 @@ const createNote = (req, res) => {
   res.json({ title, text });
 };
 
-// // This is a function called deleteNote that takes in the request and response objects as parameters.
-const deleteNote = (req, res) => {};
+// This is a function called deleteNote that takes in the request and response objects as parameters.
+const deleteNote = (req, res) => {
+  // destructure the id from the request parameters
+  const { id } = req.params;
+  // fs.readFile() method reads the notes from db.json file
+  fs.readFile(
+    // path.join() method joins the path of the current file (notesController.js) with the path to the db.json file
+    path.join(__dirname, "..", "db", "db.json"),
+    "utf8",
+    (err, notes) => {
+      // check if there is an error
+      if (err) {
+        // if there is an error, the response status is set to 500 and the error is returned as json
+        res.status(500).json({ err });
+      }
+      // parse the notes from the db.json file
+      const parsedNotes = JSON.parse(notes);
+      // filter the parsedNotes array to remove the note with the id the user sent
+      const filteredNotes = parsedNotes.filter((note) => note.id !== id);
+      // fs.writeFile() method writes the notes to db.json file
+      fs.writeFile(
+        // path.join() method joins the path of the current file (notesController.js) with the path to the db.json file
+        path.join(__dirname, "..", "db", "db.json"),
+        // JSON.stringify() method converts the filteredNotes array to JSON
+        JSON.stringify(filteredNotes, null, 2),
+        (err) => {
+          if (err) {
+            return res.status(500).json({ err });
+          }
+        }
+      );
+    }
+  );
+  // return a success message as json
+  res.json({ success: true });
+};
 
 module.exports = {
   getNotes,
